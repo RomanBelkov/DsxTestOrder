671955eed5a1767c70704485efdb4e4eb0ae5b58
Roman Belkov <romanbelkov@gmail.com>
2017-07-03 23:21:29 +0300

    adds tests for Status

--- src/test/main/kotlin/Order/Status.kt
+++ src/test/main/kotlin/Order/Status.kt
@@ -3,11 +3,13 @@ import com.jayway.restassured.builder.RequestSpecBuilder
 import com.jayway.restassured.filter.log.RequestLoggingFilter
 import com.jayway.restassured.filter.log.ResponseLoggingFilter
 import com.jayway.restassured.specification.RequestSpecification
+import org.hamcrest.Matchers.*
 import org.junit.*
 import java.util.*
 
 class StatusTest {
     private val authenticator = Authenticator()
+    private val validOrderId = 9009797
 
     companion object {
         private val resourceBundle = ResourceBundle.getBundle("keys")
@@ -25,29 +27,73 @@ class StatusTest {
 
     @Test
     fun createdOrderOK() {
-        val validOrderId = 9009797
-        val params = hashMapOf("orderId" to validOrderId, "nonce" to System.currentTimeMillis())
-        given()
-                .spec(spec)
-                .header("Sign", authenticator.getSignature(params))
-                .params(params)
-        .`when`()
-                .post()
-        .then()
-                .statusCode(200)
+        DoRequestAndAssertResult(validOrderId, 1)
     }
 
     @Test
-    fun InvalidOrderId() {
+    fun ValidOrderIdInString() {
+        DoRequestAndAssertResult(validOrderId.toString(), 1)
+    }
+
+    // WHY return code is not 400?
+    @Test
+    fun ValidOrderIdInFloat() {
+        DoRequestAndAssertResult(validOrderId.toFloat(), 0)
+    }
+
+    // WHY return code is not 400?
+    @Test
+    fun InvalidNegativeOrderId() {
         val invalidOrderId = -1
-        val params = hashMapOf("orderId" to invalidOrderId, "nonce" to System.currentTimeMillis())
+        DoRequestAndAssertResult(invalidOrderId, 0)
+    }
+
+    // WHY return code is not 400?
+    @Test
+    fun InvalidFloatOrderId() {
+        val invalidOrderId = 1.1
+        DoRequestAndAssertResult(invalidOrderId, 0)
+    }
+
+    // WHY return code is not 400?
+    @Test
+    fun InvalidBigOrderId() {
+        val invalidOrderId = System.nanoTime()
+        DoRequestAndAssertResult(invalidOrderId, 0)
+    }
+
+    // WHY return code is not 400?
+    @Test
+    fun InvalidNullOrderId() {
+        val invalidOrderId = null
+        DoRequestAndAssertResult(invalidOrderId, 0)
+    }
+
+    // WHY return code is not 400?
+    @Test
+    fun InvalidStringOrderId() {
+        val invalidOrderId = "abc"
+        DoRequestAndAssertResult(invalidOrderId, 0)
+    }
+
+    // WHY return code is not 400?
+    @Test
+    fun InvalidEmptyStringOrderId() {
+        val invalidOrderId = ""
+        DoRequestAndAssertResult(invalidOrderId, 0)
+    }
+
+    private fun DoRequestAndAssertResult(orderId : Any?, expectedSuccess : Int) {
+        val params = hashMapOf("orderId" to orderId, "nonce" to System.currentTimeMillis())
         given()
                 .spec(spec)
-                .header("Sign", authenticator.getSignature(params))
+                .header("Sign", authenticator.getSignature(resourceBundle.getString("apiSecret"), params))
                 .params(params)
         .`when`()
                 .post()
         .then()
                 .statusCode(200)
+        .assertThat()
+                .body("success", equalTo(expectedSuccess))
     }
 }
