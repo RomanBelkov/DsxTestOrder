17f8bb0fe86560fe1a6bbd4107af6c6ab9c924bb
Roman Belkov <romanbelkov@gmail.com>
2017-07-04 21:46:08 +0300

    adds Helper object

---
+++ src/test/main/kotlin/Helper.kt
@@ -0,0 +1,19 @@
+/**
+ * Created by romanbelkov on 04/07/2017.
+ */
+
+import com.jayway.restassured.builder.RequestSpecBuilder
+import com.jayway.restassured.filter.log.RequestLoggingFilter
+import com.jayway.restassured.filter.log.ResponseLoggingFilter
+import java.util.*
+
+object Helper {
+    val resourceBundle = ResourceBundle.getBundle("keys")!!
+
+    val spec = RequestSpecBuilder()
+            .setBaseUri("https://dsx.uk/tapi/v2/order")
+            .addHeader("Key", resourceBundle.getString("apiPublic"))
+            .addFilter(ResponseLoggingFilter())
+            .addFilter(RequestLoggingFilter())
+            .build()!!
+}
